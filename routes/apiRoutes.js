5b595dedde311647b720d58a18e769807335e836
Dee Lynn <deannalguzman@gmail.com>
2019-10-22 16:00:16 -0700

    fixed the api's for index.html and notes.html

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,20 +1,17 @@
-var index = require("../public/index.html");
-var notes = require("../public/notes.html");
+var noteData = require("../db/db.json");
+var path = require("path");
+
+// var index = require("../public/index.html");
+// var notes = require("../public/notes.html");
 
 module.exports = function(app) {
-  app.get("/api/index", function(req, res) {
-    res.json(index);
+  app.get("/api/notes", function(req, res) {
+    res.json(noteData);
   });
 
-  app.post("/api/index", function(req, res) {
-    if (index.length < 5) {
-      index.push(req.body);
+  app.post("/api/notes", function(req, res) {
+      noteData.push(req.body);
       res.json(true);
-    }
-    else {
-      notes.push(req.body);
-      res.json(false);
-    }
   });
 
   app.post("/api/clear", function(req, res) {
